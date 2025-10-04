cd5be6c9fb888e085f19804ffa9892dba988f3ec
Edwin Carpio <twinsanity864@hotmail.com>
2025-10-04 14:03:06 -0600

    changes back

--- SQL SCRIPTS/e_commerce.sql
+++ SQL SCRIPTS/e_commerce.sql
@@ -1,81 +1,100 @@
 CREATE DATABASE E_COMMERCE;
 USE E_COMMERCE;
+#DROP DATABASE E_COMMERCE;
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE USERS (
+    ID_USER INT AUTO_INCREMENT PRIMARY KEY,
+    USERNAME TEXT,
+    NAME TEXT NOT NULL,
+    LASTNAME TEXT NOT NULL,
+    ADDRESS TEXT NOT NULL,
+    PHONE TEXT NOT NULL,
+    EMAIL TEXT NOT NULL,
+    PASSWORD TEXT NOT NULL,
+    ROL TEXT NOT NULL,
+    PICTURE TEXT,
+    STATUS TEXT NOT NULL
 );
-#DROP TABLE usuarios;
+#DROP TABLE USERS;
 
-CREATE TABLE productos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    descripcion TEXT,
-    precio DECIMAL(10,2) NOT NULL,
-    imagen VARCHAR(255)
+CREATE TABLE PRODUCTS (
+    ID_PRODUCT INT AUTO_INCREMENT PRIMARY KEY,
+    NAME TEXT NOT NULL,
+    DESCRIPTION TEXT,
+    PRICE FLOAT NOT NULL,
+    PICTURE TEXT
 );
-#DROP TABLE productos;
+#DROP TABLE PRODUCTS;
 
-CREATE TABLE direcciones_envio (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    nombre VARCHAR(255) NOT NULL,
-    correo VARCHAR(255) NOT NULL,
-    telefono VARCHAR(20) NOT NULL,
-    direccion TEXT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+CREATE TABLE SEND_ADDRESS (
+    ID_ADDRESS INT AUTO_INCREMENT PRIMARY KEY,
+    ID_USER INT,
+    NAME TEXT NOT NULL,
+    EMAIL TEXT NOT NULL,
+    PHONE TEXT NOT NULL,
+    ADDRESS TEXT NOT NULL,
+    FOREIGN KEY (ID_USER) REFERENCES USERS(ID_USER)
 );
-#DROP TABLE direcciones_envio;
+#DROP TABLE SEND_ADDRESS;
 
-CREATE TABLE carrito (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    producto_id INT,
-    cantidad INT DEFAULT 1,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
-    FOREIGN KEY (producto_id) REFERENCES productos(id)
+CREATE TABLE CAR (
+    ID_CAR INT AUTO_INCREMENT PRIMARY KEY,
+    ID_USER INT,
+    ID_PRODUCT INT,
+    QUANTITY INT,
+    FOREIGN KEY (ID_USER) REFERENCES USERS(ID_USER),
+    FOREIGN KEY (ID_PRODUCT) REFERENCES PRODUCTS(ID_PRODUCT)
 );
-#DROP TABLE carrito;
+#DROP TABLE CAR;
 
-CREATE TABLE pedidos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario_id INT,
-    total DECIMAL(10,2),
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+CREATE TABLE ORDERS (
+    ID_ORDER INT AUTO_INCREMENT PRIMARY KEY,
+    ID_USER INT,
+    TOTAL FLOAT,
+    DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (ID_USER) REFERENCES USERS(ID)
 );
-#DROP TABLE pedidos;
+#DROP TABLE ORDERS;
 
-CREATE TABLE pedido_detalle (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    pedido_id INT,
-    producto_id INT,
-    cantidad INT NOT NULL,
-    precio DECIMAL(10, 2) NOT NULL,
-    subtotal DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
-    FOREIGN KEY (producto_id) REFERENCES productos(id)
+CREATE TABLE ORDER_DETAIL (
+    ID_DETAIL INT AUTO_INCREMENT PRIMARY KEY,
+    ID_ORDER INT,
+    ID_PRODUCT INT,
+    QUANTITY INT NOT NULL,
+    PRICE FLOAT NOT NULL,
+    SUBTOTAL FLOAT NOT NULL,
+    FOREIGN KEY (ID_ORDER) REFERENCES ORDERS(ID_ORDER),
+    FOREIGN KEY (ID_PRODUCT) REFERENCES PRODUCTS(ID_PRODUCT)
 );
-#DROP TABLE pedido_detalle;
+#DROP TABLE ORDER_DETAIL;
 
-INSERT INTO usuarios (nombre, email, password) VALUES
-('Juan Pérez', 'juan@correo.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS'),
-('María López', 'maria@correo.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS'),
-('Carlos Gómez', 'carlos@correo.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS'),
-('Edwin Lopez', 'edwin@correo.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS');
 
-INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES
-('Laptop Gamer', 'Laptop potente con procesador i7 y tarjeta gráfica RTX 3060.', 1500.00, 'laptop.jpg'),
-('Teléfono Inteligente', 'Smartphone de última generación con cámara de 108MP.', 800.00, 'telefono.jpg'),
-('Auriculares Inalámbricos', 'Auriculares con cancelación de ruido y alta fidelidad de sonido.', 120.00, 'auriculares.jpg'),
-('Monitor 4K', 'Monitor UHD 4K de 27 pulgadas con tecnología IPS.', 350.00, 'monitor.jpg'),
-('Teclado Mecánico', 'Teclado mecánico RGB con switches personalizables.', 90.00, 'teclado.jpg'),
-('Mouse Gamer', 'Mouse ergonómico con sensor óptico de alta precisión.', 60.00, 'mouse.jpg'),
-('Silla Ergonómica', 'Silla de oficina ergonómica con soporte lumbar ajustable.', 250.00, 'silla.jpg'),
-('Tablet Android', 'Tablet con pantalla de 10 pulgadas y batería de larga duración.', 400.00, 'tablet.jpg'),
-('Smartwatch', 'Reloj inteligente con monitoreo de salud y GPS integrado.', 200.00, 'smartwatch.jpg'),
-('Cámara Profesional', 'Cámara réflex digital con lente de 24MP y grabación en 4K.', 1200.00, 'camara.jpg');
+    NAME TEXT NOT NULL,
+    LASTNAME TEXT NOT NULL,
+    ADDRESS TEXT NOT NULL,
+    PHONE TEXT NOT NULL,
+    EMAIL TEXT NOT NULL,
+    PASSWORD TEXT NOT NULL,
+    ROL TEXT NOT NULL,
+    PICTURE TEXT,
+    STATUS TEXT NOT NULL
+
+INSERT INTO USERS (USERNAME, NAME, LASTNAME, ADDRESS, PHONE, EMAIL, PASSWORD, ROL, PICTURE, STATUS) VALUES
+('Juanin','Juan', 'Pérez', 'Calle #2', '3312233445', 'juan@gmail.com.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS', 'User', NULL, 'ACTIVE'),
+('Maria','María', 'López', 'Calle #2', '3312233445', 'maria@gmail.com.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS', 'User', NULL, 'ACTIVE'),
+('Carlos','Carlos', 'Gómez', 'Calle #2', '3312233445', 'carlos@gmail.com.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS', 'User', NULL, 'ACTIVE'),
+('Edwin','Edwin', 'Lopez', 'Calle #2', '3312233445', 'edwin@gmail.com.com', '$2b$10$B5gptUy41R3sw29AQlb7C.fr5C5SX9ol6va2FYuEx0keiv019/xDS', 'Administrador', NULL, 'ACTIVE');
+
+-- INSERT INTO productos (nombre, descripcion, precio, imagen) VALUES
+-- ('Laptop Gamer', 'Laptop potente con procesador i7 y tarjeta gráfica RTX 3060.', 1500.00, 'laptop.jpg'),
+-- ('Teléfono Inteligente', 'Smartphone de última generación con cámara de 108MP.', 800.00, 'telefono.jpg'),
+-- ('Auriculares Inalámbricos', 'Auriculares con cancelación de ruido y alta fidelidad de sonido.', 120.00, 'auriculares.jpg'),
+-- ('Monitor 4K', 'Monitor UHD 4K de 27 pulgadas con tecnología IPS.', 350.00, 'monitor.jpg'),
+-- ('Teclado Mecánico', 'Teclado mecánico RGB con switches personalizables.', 90.00, 'teclado.jpg'),
+-- ('Mouse Gamer', 'Mouse ergonómico con sensor óptico de alta precisión.', 60.00, 'mouse.jpg'),
+-- ('Silla Ergonómica', 'Silla de oficina ergonómica con soporte lumbar ajustable.', 250.00, 'silla.jpg'),
+-- ('Tablet Android', 'Tablet con pantalla de 10 pulgadas y batería de larga duración.', 400.00, 'tablet.jpg'),
+-- ('Smartwatch', 'Reloj inteligente con monitoreo de salud y GPS integrado.', 200.00, 'smartwatch.jpg'),
+-- ('Cámara Profesional', 'Cámara réflex digital con lente de 24MP y grabación en 4K.', 1200.00, 'camara.jpg');
 
 #DROP DATABASE E_COMMERCE;
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { login, register } = require("../controllers/auth.controller");
+const { login, register, update } = require("../controllers/auth.controller");
 
 router.post("/login", login);
-router.post("/register", register); // Opcional: Registro de usuarios
+router.post("/register", register);
+router.put("/update", update);
 
 module.exports = router;
