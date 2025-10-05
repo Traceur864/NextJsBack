9e6c2ca04954f116a7772267efdd661f83c079c1
Edwin Carpio <twinsanity864@hotmail.com>
2025-10-05 12:43:50 -0600

    Revert "changes back"
    This reverts commit cd5be6c9fb888e085f19804ffa9892dba988f3ec.

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { login, register, update } = require("../controllers/auth.controller");
+const { login, register } = require("../controllers/auth.controller");
 
 router.post("/login", login);
-router.post("/register", register);
-router.put("/update", update);
+router.post("/register", register); // Opcional: Registro de usuarios
 
 module.exports = router;
